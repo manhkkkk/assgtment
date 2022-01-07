6f6fbcdc9010a28f2643a61b50e36a43f641c0b1
manhkkkk <pktopad@gmail.com>
2022-01-07 13:33:46 +0700

    cập nhật lại menu

--- assignment/src/main.js
+++ assignment/src/main.js
@@ -2,8 +2,8 @@ const menuList =["Menu1", "Menu2", "Menu3"];
 
 const menuElement = document.querySelector('#menu');
 if (menuElement) {
-  for (let i = 0; i < menuList.length; i++) {
-    menuElement.innerHTML += '<li>'${menuList}'</li>';
+  // for (let i = 0; i < menuList.length; i++) {
+  //   menuElement.innerHTML += '<li>'${menuList}'</li>';
     
-  }
+  // }
 }
